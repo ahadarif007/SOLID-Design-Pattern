33e09ff28f4fef09b1e9e504d83ad810b2ad2d25
Abdul Ahad <ahadarif111@gmail.com>
2020-04-24 15:03:15 +0600

    initial commit

---
+++ SingleResponsibilityPrinciple.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DesignPrinciples
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento pattern!
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        // breaks single responsibility principle
+        //public void Save(string filename, bool overwrite = false)
+        //{
+        //    File.WriteAllText(filename, ToString());
+        //}
+
+        //public void Load(string filename)
+        //{
+
+        //}
+
+        //public void Load(Uri uri)
+        //{
+
+        //}
+    }
+
+    // handles the responsibility of persisting objects
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"D:\journal.txt";
+            p.SaveToFile(j, filename);
+            Process.Start(filename);
+        }
+    }
+}
